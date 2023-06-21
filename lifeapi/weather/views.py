dd6001f97b52010a70379306cffb9512d42c8938
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-06-21 08:16:47 +0300

    Render data from db, refactor views
    * reformatted the code so its easier to read
    * now possible to render the data from the db

--- lifeapi/weather/views.py
+++ lifeapi/weather/views.py
@@ -1,39 +1,52 @@
 import requests
 from django.shortcuts import render
 from datetime import datetime, timedelta
+from .models import Weather
 
 
-# Create your views here.
 def weather_view(request):
     # Get yesterday's date
     yesterday = datetime.now() - timedelta(days=1)
     date_str = yesterday.strftime("%Y-%m-%d")
 
-    # Make the API request
-    response = requests.get(
-        f"https://api.meteo.lt/v1/stations/vilniaus-ams/observations/{date_str}"
-    )
-    data = response.json()
+    # Make the API request to fetch weather data
+    api_url = f"https://api.meteo.lt/v1/stations/vilniaus-ams/observations/{date_str}"
+    response = requests.get(api_url)
+    data_fetched_from_api = response.json()
 
-    # Find the desired observation
+    # Retrieve desired observation from the fetched data
     desired_observation = None
-    for x in data["observations"]:
-        if x["observationTimeUtc"] == f"{date_str} 12:00:00":
-            desired_observation = x
+    for observation in data_fetched_from_api["observations"]:
+        if observation["observationTimeUtc"] == f"{date_str} 12:00:00":
+            desired_observation = observation
             break
 
-    # # assign the desired_observation to context
+    # Save the weather data to the database
+    save_weather_to_db(date_str, desired_observation)
+
+    # Retrieve the latest weather data from the database
+    weather_from_db = retrieve_latest_weather()
+
+    # Prepare the context to pass to the template
     context = {
         "observation": desired_observation,
+        "weather_from_db": weather_from_db,
     }
 
-    # Store only the needed values in the database
-    from .models import Weather
+    # Render the template with the context
+    return render(request, "weather/weather_template.html", context)
 
+
+def save_weather_to_db(date_str, desired_observation):
+    # Save the weather data to the database
     weather = Weather(
-        date=date_str, temperature=desired_observation.get("airTemperature", "")
+        date=date_str,
+        temperature=desired_observation.get("airTemperature", "")
     )
     weather.save()
 
-    # pass that context to the template and render it
-    return render(request, "weather/weather_template.html", context)
+
+def retrieve_latest_weather():
+    # Retrieve the latest weather data from the database
+    weather_from_db = Weather.objects.last()
+    return weather_from_db

38f648c3be6a358fd38cd1f6b0729387903ebc3d
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-06-25 20:44:18 +0300

    cleanup
    * will start with a new project and new apps

--- lifeapi_apps/weather/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Weather(models.Model):
-    date = models.DateField()
-    temperature = models.DecimalField(max_digits=5, decimal_places=2)

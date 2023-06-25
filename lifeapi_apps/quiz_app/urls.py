c5d5e1a9cedf1d21d5d5a5d05091b77bf800689a
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-06-25 22:58:18 +0300

    Quiz app connected
    * can answer the questions, stored to db
    * can create new questions, store to db
    * can navigate through everything
    
    Like the previous time, before rearranging the folders

--- lifeapi_apps/quiz_app/urls.py
+++ lifeapi_apps/quiz_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("", views.quiz_app_home, name="quiz_app_home"),
+    path("quiz/questions/", views.quiz_questions, name="quiz_questions"),
+    path('start_quiz/', views.quiz_start, name='quiz_start'),
+    path('quiz/<int:question_id>/', views.quiz_question, name='quiz_question'),
+    path('quiz/summary/', views.quiz_summary, name='quiz_summary'),
+    path('add-question/', views.quiz_add_question, name='quiz_add_question'),
 ]

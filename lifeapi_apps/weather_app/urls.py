dad7dda835e7af4005cc5f5230f56198413b956a
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-06-25 22:31:13 +0300

    Weather app plugged in
    * displays weather information from the db
    * python scrip fetches the information
    * django displays the data

--- lifeapi_apps/weather_app/urls.py
+++ lifeapi_apps/weather_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.weather_app_home, name="weather_app_home"),
+    path("fetch/", views.weather_app_display_from_db, name="weather_app_display_from_db")
 ]
--- lifeapi_apps/weather_app/views.py
+++ lifeapi_apps/weather_app/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from .models import Weather
 
 def weather_app_home(request):
     return render(request, 'weather_app_home.html')
+
+
+def weather_app_display_from_db(request):
+    # Retrieve the latest weather data from the database
+    weather_from_db = retrieve_latest_weather()
+
+    # Prepare the context to pass to the template
+    context = {
+        "weather_from_db": weather_from_db,
+    }
+
+    # Render the template with the context
+    return render(request, "weather_template.html", context)
+
+def retrieve_latest_weather():
+    # Retrieve the latest weather data from the database
+    weather_from_db = Weather.objects.last()
+    return weather_from_db

deea52d6d39673addeeb19ee44225d4153d59904
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-09-17 09:56:32 +0300

    now possible to add sequence to questions

--- lifeapi_apps/quiz_app/models.py
+++ lifeapi_apps/quiz_app/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Question(models.Model):
     description = models.TextField()
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    sequence = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return f"{self.description}"

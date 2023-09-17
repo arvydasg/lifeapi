6d328a65f292b4de6ab06728295891515b4b3e85
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-09-17 09:38:24 +0300

    HUGE change - deleted question type, learn/journal
    - simplified the code
    - not being used that much anyway
    - added Julyte, will be simplier to manage questions

--- lifeapi_apps/quiz_app/models.py
+++ lifeapi_apps/quiz_app/models.py
@@ -4,19 +4,11 @@ from django.contrib.auth.models import User
 
 
 class Question(models.Model):
-    TYPE_CHOICES = [
-        ('YN', 'Yes/No'),
-        ('Scale', 'Scale 1-5'),
-        ('Text', 'Text'),
-    ]
-
     description = models.TextField()
-    type = models.CharField(max_length=10, choices=TYPE_CHOICES, default='Text')
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
-    # change the way question object is displayed in django admin panel
     def __str__(self):
-        return f"{self.description} - {self.type}"
+        return f"{self.description}"
 
 
 class Answer(models.Model):
@@ -25,6 +17,5 @@ class Answer(models.Model):
     date_added = models.DateTimeField(default=timezone.now)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
-    # change the way answer object is displayed in django admin panel
     def __str__(self):
         return f"{self.date_added} - {self.question} - {self.answer}"

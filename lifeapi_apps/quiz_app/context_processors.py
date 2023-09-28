f8bcd53343418ffaf7a8eaf2a217ea738668780e
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-09-28 21:01:37 +0300

    data_table_test visible only to django group members
    - group called "data_table_test"

---
+++ lifeapi_apps/quiz_app/context_processors.py
@@ -0,0 +1,11 @@
+# making variables available in ALL requests
+
+def data_table_test_status(request):
+    # Check if the user is a member of the "data_table_test" group
+    is_data_table_test_member = request.user.groups.filter(name='data_table_test').exists()
+
+    print(is_data_table_test_member)
+
+    return {
+        'is_data_table_test_member': is_data_table_test_member,
+    }

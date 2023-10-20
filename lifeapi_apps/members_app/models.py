c2e2609e96185520aa3761d78848f5a976909143
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-10-20 05:54:42 +0300

    Profile model assigns User db entry with rescuetime_api_key entry
    - tutorial that I followed - https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
    - I can now place rescuetime apie key for each user in the database instead of .env file

--- lifeapi_apps/members_app/models.py
+++ lifeapi_apps/members_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+# created according to this tutorial :
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rescuetime_api_key = models.TextField(max_length=100, blank=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.rescuetime_api_key}"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()

f5c00e3425701e4bbe7515b056850ee79802b20c
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-10-21 08:16:30 +0300

    updated weather files so they are able to reach the settings file

--- jobs/dev/weather_job_dev.py
+++ jobs/dev/weather_job_dev.py
@@ -1,9 +1,13 @@
-# Configure Django settings for script execution outside of a Django project
-# This enables the proper functioning of Django-related features, including database access via models
-
 import os
 import django
 
+# appending the path of the project's root directory to sys.path, 
+# ensuring that Python can locate the settings module when you run the script from a different directory.
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+# Configure Django settings for script execution outside of a Django project
+# This enables the proper functioning of Django-related features, including database access via models
 # Set the DJANGO_SETTINGS_MODULE environment variable
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.development")
 # Configure Django settings
